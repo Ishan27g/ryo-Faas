f2465afb225320287afb166caa39edb588c21c26
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2022-03-30 03:19:44 +1100

    oops

---
+++ database/handler/handler.go
@@ -0,0 +1,204 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	database "github.com/Ishan27g/ryo-Faas/database/db"
+	deploy "github.com/Ishan27g/ryo-Faas/pkg/proto"
+	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+)
+
+var db = database.GetDatabase()
+
+type handle struct {
+	Gin *gin.Engine
+	Rpc rpc
+}
+type rpc struct{}
+
+func toStoreDoc(document *deploy.Document) database.NatsDoc {
+	data := make(map[string]interface{})
+	err := json.Unmarshal(document.Data, &data)
+	if err != nil {
+		fmt.Println("toStoreDoc", err.Error())
+		return nil
+	}
+	return database.NewDocument(document.Table, document.Id, data)
+}
+
+func (d *rpc) forEachDoc(documents *deploy.Documents, cb func(data database.NatsDoc)) (*deploy.Ids, error) {
+	var ids = new(deploy.Ids)
+	var err error
+	for _, document := range documents.Document {
+		if doc := toStoreDoc(document); doc != nil {
+			cb(doc)
+			ids.Id = append(ids.Id, doc.Id())
+		} else {
+			return nil, err
+		}
+	}
+	return ids, nil
+}
+func (d *rpc) New(ctx context.Context, documents *deploy.Documents) (*deploy.Ids, error) {
+	return d.forEachDoc(documents, func(doc database.NatsDoc) {
+		db.New(doc)
+	})
+}
+
+func (d *rpc) Update(ctx context.Context, documents *deploy.Documents) (*deploy.Ids, error) {
+	return d.forEachDoc(documents, func(doc database.NatsDoc) {
+		db.Update(doc)
+	})
+}
+
+func (d *rpc) Get(ctx context.Context, ids *deploy.Ids) (*deploy.Documents, error) {
+	if len(ids.Id) == 0 {
+		return d.All(ctx, ids)
+	}
+	var documents []*deploy.Document
+	for _, id := range ids.Id {
+		if entity := db.Get(id); entity != nil {
+			data, _ := json.Marshal(entity)
+			documents = append(documents, &deploy.Document{
+				Id:   entity.Id,
+				Data: data,
+			})
+		}
+	}
+	return &deploy.Documents{Document: documents}, nil
+}
+
+func (d *rpc) Delete(ctx context.Context, ids *deploy.Ids) (*deploy.Ids, error) {
+	for _, id := range ids.Id {
+		db.Delete(id)
+	}
+	return &deploy.Ids{Id: ids.Id}, nil
+}
+
+func (d *rpc) All(ctx context.Context, ids *deploy.Ids) (*deploy.Documents, error) {
+	var documents []*deploy.Document
+	for _, entity := range db.All() {
+		data, _ := json.Marshal(*entity)
+		documents = append(documents, &deploy.Document{
+			Id:   entity.Id,
+			Data: data,
+		})
+	}
+	return &deploy.Documents{Document: documents}, nil
+}
+func (d *handle) AddHttp(c *gin.Context) {
+	doc, valid := d.isValid(c)
+	if !valid {
+		c.JSON(400, nil)
+		return
+	}
+	db.New(doc)
+	c.JSON(http.StatusCreated, doc.Id())
+}
+func (d *handle) UpdateHttp(c *gin.Context) {
+	id, found := c.Params.Get("id")
+	if !found {
+		c.JSON(400, nil)
+		return
+	}
+	doc, valid := d.isValid(c, id)
+	if !valid {
+		c.JSON(400, nil)
+		return
+	}
+	fmt.Println(doc)
+	db.Update(doc)
+	c.JSON(http.StatusOK, doc.Id())
+}
+func (d *handle) DeleteHttp(c *gin.Context) {
+	id, found := c.Params.Get("id")
+	if !found {
+		c.JSON(400, nil)
+		return
+	}
+	db.Delete(id)
+	c.JSON(http.StatusOK, id)
+}
+func (d *handle) GetHttp(c *gin.Context) {
+	id, found := c.Params.Get("id")
+	if !found {
+		fmt.Println("no id", c.Request.URL)
+		c.JSON(400, nil)
+		return
+	}
+	entity := db.Get(id)
+	c.JSON(http.StatusOK, *entity)
+}
+func (d *handle) AfterHttp(c *gin.Context) {
+	id, found := c.Params.Get("time")
+	if !found {
+		c.JSON(400, nil)
+		return
+	}
+	entities := db.After(id)
+	var documents []database.Entity
+	for _, entity := range entities {
+		documents = append(documents, *entity)
+	}
+	c.JSON(http.StatusOK, entities)
+}
+func (d *handle) AllHttp(c *gin.Context) {
+	var docs []database.Entity
+	for _, v := range db.All() {
+		docs = append(docs, (*v))
+	}
+	c.JSON(http.StatusOK, docs)
+}
+func (d *handle) AllHttpIds(c *gin.Context) {
+	var docs []string
+	for _, v := range db.All() {
+		docs = append(docs, (*v).Id)
+	}
+	c.JSON(http.StatusOK, docs)
+}
+
+type Document struct {
+	Table string                 `json:"Table"`
+	Data  map[string]interface{} `json:"Data"`
+}
+
+func (*handle) isValid(c *gin.Context, id ...string) (database.NatsDoc, bool) {
+	var data Document
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, false
+	}
+	doc := database.FromJson(data.Table, data.Data, id...)
+	return doc, true
+}
+func GetHandler() handle {
+	h := handle{
+		nil,
+		rpc{},
+	}
+	gin.SetMode(gin.ReleaseMode)
+	h.Gin = gin.New()
+	h.Gin.Use(gin.Recovery())
+	h.Gin.Use(func(ctx *gin.Context) {
+		fmt.Println(fmt.Sprintf("[%s] [%s]", ctx.Request.Method, ctx.Request.RequestURI))
+		ctx.Next()
+	})
+	h.Gin.Use(otelgin.Middleware("database"))
+	g := h.Gin.Group("/database")
+	{
+		g.GET("/get/:id", h.GetHttp)
+		g.GET("/all", h.AllHttp)
+		g.GET("/after/:time", h.AfterHttp)
+		g.GET("/ids", h.AllHttpIds)
+		g.POST("/new", h.AddHttp)
+		g.PATCH("/update/:id", h.UpdateHttp)
+		g.DELETE("/delete/:id", h.DeleteHttp)
+	}
+	return h
+
+}
---
+++ database/handler/handler_test.go
@@ -0,0 +1,167 @@
+package handler
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	client "github.com/Ishan27g/ryo-Faas/database/client"
+	database "github.com/Ishan27g/ryo-Faas/database/db"
+	deploy "github.com/Ishan27g/ryo-Faas/pkg/proto"
+	"github.com/Ishan27g/ryo-Faas/store"
+
+	"github.com/Ishan27g/ryo-Faas/pkg/transport"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+var id string
+var entity database.Entity
+var data = map[string]interface{}{
+	"Num":  1,
+	"From": 300,
+	"To":   302,
+}
+var table1 = map[string]interface{}{
+	"Table": "ok",
+	"Data":  data,
+}
+var table2 = map[string]interface{}{
+	"Table": "ok2",
+	"Data":  data,
+}
+
+func Test_Grpc(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var handler = GetHandler()
+
+	config := []transport.Config{transport.WithRpcPort(":5001"), transport.WithDatabaseServer(&handler.Rpc)}
+	transport.Init(ctx, config...).Start()
+
+	<-time.After(5 * time.Second)
+
+	c := client.Connect("localhost:5001")
+
+	d, err := json.Marshal(table1["Data"])
+	assert.NoError(t, err)
+
+	id, err := c.New(ctx, &deploy.Documents{Document: []*deploy.Document{{Table: table1["Table"].(string), Id: "", Data: d}}})
+	docId := id.GetId()[0]
+	assert.NoError(t, err)
+
+	doc, err := c.Get(ctx, &deploy.Ids{Id: id.Id})
+
+	docs := store.ToDocs(doc, "Table")
+
+	for _, d := range docs {
+		assert.Equal(t, docId, d.Id)
+		assert.NotNil(t, d.Data.Value["Num"])
+		assert.NotNil(t, d.Data.Value["From"])
+		assert.NotNil(t, d.Data.Value["To"])
+	}
+
+	deletedIds, err := c.Delete(ctx, &deploy.Ids{Id: id.Id})
+	assert.NoError(t, err)
+	assert.Equal(t, docId, deletedIds.GetId()[0])
+
+}
+func Test_Http(t *testing.T) {
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	payload := new(bytes.Buffer)
+	json.NewEncoder(payload).Encode(table2)
+
+	h := GetHandler()
+	{
+		// add
+		c.Request = httptest.NewRequest("POST", "/add", payload)
+
+		h.AddHttp(c)
+		b, _ := ioutil.ReadAll(w.Body)
+		if w.Code != http.StatusCreated {
+			t.Error(w.Code, " ❌ "+string(b))
+		}
+		id = strings.Trim(string(b), "\"")
+		fmt.Println(" ✅ Added", id)
+	}
+	{
+		// get id
+		w = httptest.NewRecorder()
+		c, _ = gin.CreateTestContext(w)
+		c.Params = []gin.Param{{Key: "id", Value: id}}
+
+		h.GetHttp(c)
+		b, _ := ioutil.ReadAll(w.Body)
+		if w.Code != http.StatusOK {
+			t.Error(w.Code, " ❌ "+string(b))
+		}
+		assert.NoError(t, json.Unmarshal(b, &entity))
+		fmt.Println(" ✅ Retrieved entity", entity)
+	}
+	{
+		// get all
+		c.Request = httptest.NewRequest("GET", "/all", nil)
+		h.AllHttp(c)
+		b, _ := ioutil.ReadAll(w.Body)
+		if w.Code != http.StatusOK {
+			t.Error(w.Code, " ❌ "+string(b))
+		}
+		var entities []database.Entity
+		json.Unmarshal(b, &entities)
+		fmt.Println(" ✅ Retrieved entities", entities)
+		assert.Equal(t, entity, entities[0])
+	}
+	{
+		//  all ids
+		c.Request = httptest.NewRequest("GET", "/all", nil)
+		h.AllHttpIds(c)
+		b, _ := ioutil.ReadAll(w.Body)
+		if w.Code != http.StatusOK {
+			t.Error(w.Code, " ❌ "+string(b))
+		}
+		var ids []string
+		json.Unmarshal(b, &ids)
+
+		fmt.Println(" ✅ Retrieved ids", ids)
+		assert.Equal(t, id, ids[0])
+	}
+	{
+		//  delete
+		// c, _ = gin.CreateTestContext(w)
+		c.Params = []gin.Param{{Key: "id", Value: id}}
+
+		h.DeleteHttp(c)
+		b, _ := ioutil.ReadAll(w.Body)
+		if w.Code != http.StatusOK {
+			t.Error(w.Code, " ❌ "+string(b))
+		}
+		deletedId := strings.Trim(string(b), "\"")
+		assert.Equal(t, id, deletedId)
+		fmt.Println(" ✅ Deleted", id)
+	}
+	{
+		// get all
+		//c.Request = httptest.NewRequest("GET", "/all", nil)
+		h.AllHttp(c)
+		b, _ := ioutil.ReadAll(w.Body)
+		if w.Code != http.StatusOK {
+			t.Error(w.Code, " ❌ "+string(b))
+		}
+		var entities []database.Entity
+		json.Unmarshal(b, &entities)
+		assert.Empty(t, entities)
+		fmt.Println(" ✅ Is empty")
+
+	}
+}
