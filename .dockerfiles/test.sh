6253480d46e65032c4e6497cbff9c3e94a777e4a
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2022-12-16 19:51:26 +1100

    fix test.sh

--- .dockerfiles/test.sh
+++ .dockerfiles/test.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-  echo ""
+  echo "NOT BUILDING IMAGES FOR TEST!!!!!!"
 else
-  task imgProxy
+  task build
 fi
 
-go run cli.go sto
-rm -rf /Users/ishan/Documents/Drive/golang/ryo-Faas/
-rm -rf /Users/ishan/Desktop/multi/*
-cp -r examples/* /Users/ishan/Desktop/multi/
-go run cli.go init
-go run cli.go sta
-
-go run cli.go deploy pkg/scale/deploy-scale.json
-go run cli.go deploy examples/deploy-otel.json
-
-#go run cli.go deploy examples/deploy-otel.json
-#go run cli.go deploy examples/deploy-otel.json
+# test as ryo-faas `command`, without taskfile env
+go build -o ryo-Faas cli.go
+./ryo-Faas sto
+./ryo-Faas i
+./ryo-Faas sta
+./ryo-Faas deploy --main examples/deployMain.json
+curl http://localhost:9999/functions/database-events/pay
+curl http://localhost:9999/functions/database-events/pay
+curl http://localhost:9999/functions/database-events/pay
+curl http://localhost:9999/functions/database-events/pay
+curl http://localhost:9999/functions/database-events/get
